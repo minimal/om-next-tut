4b21f1da6f63a507443d6e07caef1ad5b8e91dd6
Christopher McDevitt <christophermcdevitt@gmail.com>
2015-10-17 15:36:38 +0100

    publish: update om version, use local snapshot of devcards
    generated from commit 1692749761dfab1f848453d7a33ed1179d9b7e37

--- js/figwheel/connect.cljs
+++
@@ -1,4 +0,0 @@
-(ns figwheel.connect (:require [devcards.core :include-macros true] [om-next-tut.core] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:devcards true, :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-(devcards.core/start-devcard-ui!)
-
